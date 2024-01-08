5495e1bfefc3b33f0df0b53c66a3b1daf447aa0c
Quan <cho.quan025@gmail.com>
2024-01-07 18:14:53 -0600

    feat: threading api

--- ostep-homework/threads-api/main-race.c
+++ ostep-homework/threads-api/main-race.c
@@ -2,17 +2,23 @@
 
 #include "common_threads.h"
 
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+
 int balance = 0;
 
 void* worker(void* arg) {
+    Pthread_mutex_lock(&m1);
     balance++; // unprotected access 
+    Pthread_mutex_unlock(&m1);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
+    Pthread_mutex_lock(&m1);
     balance++; // unprotected access
+    Pthread_mutex_unlock(&m1);
     Pthread_join(p, NULL);
     return 0;
 }
